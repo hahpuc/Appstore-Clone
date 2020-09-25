1b3650458fd0fcb4cc00573a929d30dc20e31972
longnguyen <18520092@gm.uit.edu.vn>
2020-09-25 12:12:17 +0700

    SearchView: Create Cells and UIStackView Layout

--- Appstore-Clone/Controller/AppSearchController.swift
+++ Appstore-Clone/Controller/AppSearchController.swift
@@ -7,12 +7,35 @@
 
 import UIKit
 
-class AppSearchController: UICollectionViewController {
+class AppSearchController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    
+    fileprivate let cellID = "cell111"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        collectionView.backgroundColor = .red
+        collectionView.backgroundColor = .white
+        
+        collectionView.register(SearchResultCell.self, forCellWithReuseIdentifier: cellID)
     }
+    
+    // Add UICollectionViewDelegateFlowLayout to access this func
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        return .init(width: view.frame.width, height: 250)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellID, for: indexPath)
+        
+        return cell
+    }
+    
     
     init() {
         super.init(collectionViewLayout: UICollectionViewFlowLayout())
